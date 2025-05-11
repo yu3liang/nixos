c7dc6636ef8fc5664ce570a3d8f15fba0e653c60
yu3liang <gustavo.lampert@gmail.com>
2025-05-11 20:32:38 -0300

    Added flakes

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,8 @@
       ./hardware-configuration.nix
     ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -102,8 +104,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   git
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+  wget
+  vim
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
