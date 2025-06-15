58456303521bf17530216464fea416c1cc7e55a1
yu3liang <gustavo.lampert@gmail.com>
2025-06-15 17:43:44 -0300

    nixos.nix formatting

--- configs/nixos.nix
+++ configs/nixos.nix
@@ -5,31 +5,34 @@ let
 in
 
 {
-  ############################################################### BOOTLOADER #####################################################################
-  ################################################################################################################################################
+  # <----BOOTLOADER---->
 
-  # Enable the systemd-boot bootloader, which is a simple UEFI boot manager.
+  # Enable the systemd-boot bootloader (UEFI boot manager)
   boot.loader.systemd-boot.enable = true;
   
-  # Allow NixOS to write to EFI variables, which is required for managing UEFI boot entries (needed for systemd-boot and other UEFI bootloaders).
+  # Allow NixOS to write to EFI variables (required for managing UEFI boot entries, needed for systemd-boot and other UEFI bootloaders)
   boot.loader.efi.canTouchEfiVariables = true;
 
-  ################################################################################################################################################
-  ################################################################################################################################################
 
-  # ----------------------------------------------------------------------------------------------------------------------------------------------
+  # <----NETWORKING---->
 
-  ########################### NETWORKING ############################
-  ###################################################################
-
-  # Set the system hostname.
+  # Set the system hostname
   networking.hostName = vars.desktop;
-
-  # Enable DHCP for network interfaces
-  # networking.useDHCP = lib.mkDefault true;
 
   # Enable network manager
   networking.networkmanager.enable = true;
+
+  # Enable firewall
+  networking.firewall.enable = true;
+
+  # Open ports in the firewall
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether
+  # networking.firewall.enable = false;
+
+  # Enable DHCP for network interfaces
+  # networking.useDHCP = lib.mkDefault true;
   
   # Wireless support via wpa_supplicant
   # networking.wireless.enable = true;
@@ -38,27 +41,13 @@ in
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable firewall
-  networking.firewall.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # <----LOCALIZATION---->
 
-  ###################################################################
-  ###################################################################
-
-  # -----------------------------------------------------------------
-
-  ########################### LOCALIZATION #############################
-  ######################################################################
-
-  # Set your system timezone.
+  # Set your system timezone
   time.timeZone = "America/Sao_Paulo";
 
-  # Set the default system language/locale to US English.
+  # Set the default system language/locale 
   i18n.defaultLocale = "en_US.UTF-8";
   
   # Set specific locale categories (address, measurement, money, etc.)
@@ -77,67 +66,60 @@ in
   # Set the console keyboard layout
   console.keyMap = "us-acentos";
 
-  ######################################################################
-  ######################################################################
 
-  # --------------------------------------------------------------------
-
-  ######################################################## AUDIO ##########################################################
-  #########################################################################################################################
-
-  # Disable the legacy PulseAudio server.
-  services.pulseaudio.enable = false;
-
-  # Enable the real-time kit daemon, which allows audio processes to get real-time CPU scheduling (reduces audio glitches).
-  security.rtkit.enable = true;
+  # <----AUDIO---->
 
   services.pipewire = {
     # Enable PipeWire
     enable = true;
 
-    # Enable ALSA (Advanced Linux Sound Architecture) support.
+    # Enable ALSA (Advanced Linux Sound Architecture) support
     alsa.enable = true;
 
-    # Add 32-bit ALSA support (for compatibility with some apps/games).
+    # Add 32-bit ALSA support (for compatibility with some apps/games)
     alsa.support32Bit = true;
 
-    # Provide a PulseAudio-compatible server via PipeWire (so apps expecting PulseAudio still work).
+    # Provide a PulseAudio-compatible server via PipeWire (so apps expecting PulseAudio can still work)
     pulse.enable = true;
     
-    # Enable JACK support for professional audio.
+    # Enable JACK support for professional audio
     # jack.enable = true;
   };
-  #########################################################################################################################
-  #########################################################################################################################
 
-  # -----------------------------------------------------------------------------------------------------------------------
+  # Disable the legacy PulseAudio server
+  services.pulseaudio.enable = false;
 
-  ################################# VIDEO ###################################
-  ###########################################################################
+  # Enable the real-time kit daemon (allows audio processes to get real-time CPU scheduling and reduces audio glitches)
+  security.rtkit.enable = true;
 
-  # Enable general graphics hardware support.
+
+  # <----VIDEO---->
+
+  # Enable general graphics hardware support
   hardware.graphics.enable = true;
+
+  # <----VIDEO (NVIDIA GPU)---->
   
-  # Set the X server to use the NVIDIA driver.
+  # Set the X server to use NVIDIA driver
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
-    # Enable kernel mode-setting for NVIDIA (required for some features).
+    # Enable kernel mode-setting for NVIDIA (required for some features)
     modesetting.enable = true;
     
-    # Disable fine-grained power management.
+    # Disable fine-grained power management
     powerManagement = {
       enable = false;
       finegrained = false;
     };
     
-    # Use the proprietary (not open source) NVIDIA driver.
+    # Use the proprietary (not open source) NVIDIA driver
     open = false;
 
-    # Install the NVIDIA settings tool.
+    # Install the NVIDIA settings tool
     nvidiaSettings = true;
     
-    # Use the stable version of the NVIDIA driver package.
+    # Use the stable version of the NVIDIA driver package
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
@@ -147,109 +129,90 @@ in
     "VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE" = "HDMI-0";
   };
 
-  ###########################################################################
-  ###########################################################################
 
-  # -------------------------------------------------------------------------
-
-  ############################## BLUETOOTH #################################
-  ##########################################################################
+  # <----BLUETOOTH---->
 
   # Graphical Bluetooth manager (GUI)
   services.blueman.enable = true;
 
   hardware = {
-    # Enable Bluetooth hardware support.
+    # Enable Bluetooth hardware support
     bluetooth.enable = true;
 
-    # Turn on Bluetooth automatically at boot.
+    # Turn on Bluetooth automatically at boot
     bluetooth.powerOnBoot = true;
 
     bluetooth = {
       settings = {
         General = {
-          # Support both BLE (Bluetooth Low Energy) and classic Bluetooth.
+          # Support both BLE (Bluetooth Low Energy) and classic Bluetooth
           ControllerMode = "dual";
 
-          # Allow devices to connect more quickly.
+          # Allow devices to connect more quickly
           FastConnectable = "true";
 
-          # Enable experimental Bluetooth features.
+          # Enable experimental Bluetooth features
           Experimental = "true";
         };
         
         Policy = {
-          # Automatically enable Bluetooth adapters when detected.
+          # Automatically enable Bluetooth adapters when detected
           AutoEnable = "true";
         };
       };
     };
   };
 
-  ##########################################################################
-  ##########################################################################
 
-  # ------------------------------------------------------------------------
+  # <----PRINTING---->
 
-  ########################## PRINTING ############################
-  ################################################################
-
-  # Turn on the printing service (CUPS) to add and use printers.
+  # Turn on the printing service (CUPS) to add and use printers
   services.printing.enable = true;
 
-  ################################################################
-  ################################################################
 
-  # --------------------------------------------------------------
-
-  ################################ SERVICES ###################################
-  #############################################################################
+  # <----SERVICES---->
 
   services = {
     xserver = {
-      # Enable the X11 window system.
+      # Enable the X11 window system
       enable = true;
 
-      # Set the keyboard layout to US.
+      # Set the keyboard layout
       xkb.layout = "us";
-      # Set the keyboard variant to "intl" (international).
+      # Set the keyboard variant
       xkb.variant = "intl";
 
-      # Exclude xterm (terminal emulator) from being installed.
+      # Exclude xterm (terminal emulator) from being installed
       excludePackages = [ pkgs.xterm ];
     };
 
-    # Enable firmware updates for hardware (e.g. BIOS, peripherals).
+    # Enable firmware updates for hardware (e.g. BIOS, peripherals)
     fwupd.enable = true;
 
-    # Enable thumbnail generation for files (used by file managers).
+    # Enable thumbnail generation for files (used by file managers)
     tumbler.enable = true;
     
-    # Provide virtual filesystem support (e.g., mounting remote filesystems).
+    # Provide virtual filesystem support (e.g., mounting remote filesystems)
     gvfs.enable = true;
     
-    # Enable Flatpak, a system for installing sandboxed apps.w
+    # Enable Flatpak (system for installing sandboxed apps)
     flatpak.enable = true;
 
-    # Disable the power profiles daemon (used for power management).
+    # Disable the power profiles daemon (used for power management)
     # power-profiles-daemon.enable = false;
-    # Power management daemon (battery status, suspend, etc.).
+    # Power management daemon (battery status, suspend, etc.)
     # upower.enable = true;
-    # Enable ACPI daemon for handling power events (lid close, etc.).
+    # Enable ACPI daemon for handling power events (lid close, etc.)
     # acpid.enable = true;
   };
-  #############################################################################
-  #############################################################################
 
-  # ---------------------------------------------------------------------------
 
-  ########################################################## SYSTEM ###############################################################
-  #################################################################################################################################
+  # <----SYSTEM---->
 
-  # Allow installation of unfree (proprietary) packages from Nixpkgs.
+  # Allow installation of unfree (proprietary) packages from Nixpkgs
   nixpkgs.config.allowUnfree = true;
 
-  # Enable the OpenSSH server for remote access.
+  # Enable the OpenSSH server for remote access
   services.openssh = {
     enable = true;
     settings = {
@@ -258,12 +221,6 @@ in
     };
   };
 
-  # Set the NixOS system state version. This should match your NixOS release and only be changed when upgrading to a new release.
+  # Set the NixOS system state version. This should match your NixOS release and only be changed when upgrading to a new release
   system.stateVersion = "24.11"; # Don't change unless necessary
-
-  #################################################################################################################################
-  #################################################################################################################################
-
-  # -------------------------------------------------------------------------------------------------------------------------------
-  
 }
